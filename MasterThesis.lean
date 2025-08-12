849fed21d4a92cac165dfd45eaefcd86ceb0a1e1
Madeleine Gignoux <madeleine.gignoux@gmail.com>
2025-08-12 15:18:27 +0200

    Fix build

--- MasterThesis.lean
+++ MasterThesis.lean
@@ -1,2 +1,7 @@
+import GL.CoalgebraProof
+import GL.Graphviz
+import GL.Logic
+import GL.SplitCoalgebraProof
+
 -- This module serves as the root of the `MasterThesis` library.
 -- Import modules here that should be built as part of the library.
