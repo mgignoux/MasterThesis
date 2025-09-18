73b42a5aa47971ac030b6ae16c745ff7bf00c3d5
Madeleine Gignoux <madeleine.gignoux@gmail.com>
2025-09-18 19:19:58 +0200

    Add fresh variables to system of equations

--- GL/Interpolation.lean
+++ GL/Interpolation.lean
@@ -1,10 +1,11 @@
 import GL.Logic
 import GL.CoalgebraProof
 import GL.SplitCoalgebraProof
+import Mathlib.Data.Fintype.Defs
 
 /- SYSTEMS OF EQUATIONS -/
 
-/- Structure preserving map substituting Pₙ by C -/
+/-- Structure preserving map substituting Pₙ by C --/
 def single (n : Nat) (C : Formula) : Formula → Formula
   | ⊥ => ⊥
   | ⊤ => ⊤
@@ -15,7 +16,7 @@ def single (n : Nat) (C : Formula) : Formula → Formula
   | □ A => □ (single n C A)
   | ◇ A => ◇ (single n C A)
 
-/- Structure preserving map substituting all atoms meeting a certain criteria p -/
+/-- Structure preserving map substituting all atoms meeting a certain criteria p --/
 def partial_ (p : Nat → Prop) [DecidablePred p] (σ : Subtype p → Formula) : Formula → Formula
   | ⊥ => ⊥
   | ⊤ => ⊤
@@ -26,7 +27,7 @@ def partial_ (p : Nat → Prop) [DecidablePred p] (σ : Subtype p → Formula) :
   | □ A => □ (partial_ p σ A)
   | ◇ A => ◇ (partial_ p σ A)
 
-/- Structure preserving map substituting all atoms via a transformation σ -/
+/-- Structure preserving map substituting all atoms via a transformation σ --/
 def full (σ : Nat → Formula) (A : Formula) : Formula := match A with
   | ⊥ => ⊥
   | ⊤ => ⊤
@@ -42,22 +43,103 @@ decreasing_by
   simp [Formula.size]
   try linarith
 
-/- Shift all atoms up or down, might come in handy later -/
+/- Shift all atoms up or down, might come in handy later --/
 def shift_up (k : Nat) : Formula → Formula := full (λ n ↦ at (n + k))
 def shift_down (k : Nat) : Formula → Formula := full (λ n ↦ at (n - k))
 
 namespace split
 
-def equation (𝕏 : GLSplitProof) {n : Nat} (bij : 𝕏.X ≃ Fin n) (x : 𝕏.X) : Formula := match r 𝕏.α x with
+def List.get_singleton {α : Type} (l : List α) (h : ∃ x, [x] = l) : α := match l with
+  | [x] => x
+  | [] => by exfalso; grind
+  | x::y::xs => by exfalso; grind
+
+-- universe u
+-- inductive Formula_on {X : Type} : (vars : List X) → Type
+--   | var {vars} : (x : X) → (h : vars = []) → Formula_on (x :: vars)
+--   | bottom {vars} : Formula_on vars
+--   | top {vars} : Formula_on vars
+--   | atom {vars} : Nat → Formula_on vars
+--   | neg_atom {vars} : Nat → Formula_on vars
+--   | and {vars} : Formula_on vars → Formula_on vars → Formula_on vars
+--   | or {vars} : Formula_on vars → Formula_on vars → Formula_on vars
+--   | box {vars} : Formula_on vars → Formula_on vars
+--   | diamond {vars} : Formula_on vars → Formula_on vars
+-- deriving Repr,DecidableEq
+
+-- def coe {X : Type} : Formula_on ([] : List X) → Formula
+--   | .bottom => ⊥
+--   | .top => ⊤
+--   | .atom n => at n
+--   | .neg_atom n => na n
+--   | .and A B => (coe A) & (coe B)
+--   | .or A B => (coe A) v (coe B)
+--   | .box A => □ (coe A)
+--   | .diamond A => ◇ (coe A)
+
+-- def eoc {X : Type} : Formula → Formula_on ([] : List X)
+--   | .bottom => .bottom
+--   | .top => .top
+--   | .atom n => .atom n
+--   | .neg_atom n => .neg_atom n
+--   | .and A B => .and (eoc A) (eoc B)
+--   | .or A B => .or (eoc A) (eoc B)
+--   | .box A => .box (eoc A)
+--   | .diamond A => .diamond (eoc A)
+
+-- instance {X : Type} : Equiv (Formula_on ([] : List X)) (Formula) where
+--   toFun := coe
+--   invFun := eoc
+--   left_inv := by sorry
+--   right_inv := by sorry
+
+
+-- def equation (𝕏 : GLSplitProof) [fin : Fintype 𝕏.X] (x : 𝕏.X) : Formula_on fin.elems := match r 𝕏.α x with
+
+
+def GLSplitProof.Sequent (𝕏 : GLSplitProof) [fin_X : Fintype 𝕏.X] : Finset Formula :=
+  fin_X.elems.biUnion (fun x ↦ (f 𝕏.α x).image (Sum.elim id id))
+
+def GLSplitProof.freeVar (𝕏 : GLSplitProof) [fin_X : Fintype 𝕏.X] : Nat :=
+  Formula.sequent.Sequent.freshVar (GLSplitProof.Sequent 𝕏)
+
+def GLSplitProof.Encode (𝕏 : GLSplitProof) [fin_X : Fintype 𝕏.X] {n : Nat} (bij : 𝕏.X ≃ Fin n) (x : 𝕏.X) :=
+  GLSplitProof.freeVar 𝕏 + bij.toFun x
+
+def equation (𝕏 : GLSplitProof) {n : Nat} [fin_X : Fintype 𝕏.X] (bij : 𝕏.X ≃ Fin n) (x : 𝕏.X) : Formula := match r : r 𝕏.α x with
   | RuleApp.topₗ => ⊥
   | RuleApp.topᵣ => ⊤
   | RuleApp.axₗₗ _ => ⊥
-  | RuleApp.axₗᵣ k => na (k + n) -- probably shift this up by n to avoid issues later
-  | RuleApp.axᵣₗ k => at (k + n)
+  | RuleApp.axₗᵣ k => na k
+  | RuleApp.axᵣₗ k => at k
   | RuleApp.axᵣᵣ _ => ⊤
-  | RuleApp.orₗ A B => at (bij.toFun (by sorry))
-  | RuleApp.orᵣ A B => by sorry
-  | RuleApp.andₗ A B => by sorry
-  | RuleApp.andᵣ A B => by sorry
-  | RuleApp.boxₗ A => by sorry
-  | RuleApp.boxᵣ A => by sorry
+  | RuleApp.orₗ A B => match p : p 𝕏.α x with
+    | [] => by exfalso ; sorry
+    | [y] => at (GLSplitProof.Encode 𝕏 bij y)
+    | x :: y :: xs => by exfalso; sorry
+  | RuleApp.orᵣ A B => match p 𝕏.α x with
+    | [] => by exfalso ; sorry
+    | [y] => at (GLSplitProof.Encode 𝕏 bij y)
+    | x :: y :: xs => by exfalso; sorry
+  | RuleApp.andₗ A B => match p 𝕏.α x with
+    | [] => by exfalso ; sorry
+    | [y] => by exfalso ; sorry
+    | x :: y :: [] => at (GLSplitProof.Encode 𝕏 bij x) v at (GLSplitProof.Encode 𝕏 bij y)
+    | x :: y :: z :: xs => by exfalso; sorry
+  | RuleApp.andᵣ A B => match p 𝕏.α x with
+    | [] => by exfalso ; sorry
+    | [y] => by exfalso ; sorry
+    | x :: y :: [] => at (GLSplitProof.Encode 𝕏 bij x) & at (GLSplitProof.Encode 𝕏 bij y)
+    | x :: y :: z :: xs => by exfalso; sorry
+  | RuleApp.boxₗ A => match p 𝕏.α x with
+    | [] => by exfalso ; sorry
+    | [y] => ◇ at (GLSplitProof.Encode 𝕏 bij y)
+    | x :: y :: xs => by exfalso; sorry
+  | RuleApp.boxᵣ A => match p 𝕏.α x with
+    | [] => by exfalso ; sorry
+    | [y] => □ at (GLSplitProof.Encode 𝕏 bij y)
+    | x :: y :: xs => by exfalso; sorry
+
+theorem existsSolution (𝕏 : GLSplitProof) [fin_X : Fintype 𝕏.X] {n : Nat} (bij : 𝕏.X ≃ Fin n) :
+  ∃ σ : Nat → Formula,
+    (∀ x : 𝕏.X, σ (GLSplitProof.Encode 𝕏 bij x) ≅ full σ (equation 𝕏 bij x)) := by sorry
